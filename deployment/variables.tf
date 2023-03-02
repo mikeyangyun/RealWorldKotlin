a35e23c468299fcb37441d737ab26366dadc5e85
Yunsong <Mikeyangyun@gmail.com>
2023-03-03 00:31:27 +0800

    [yunsong] feat: add logs and variables for infra

---
+++ deployment/variables.tf
@@ -0,0 +1,20 @@
+# provider variable
+variable "aws_region" {
+  description = "Configuring AWS as provider"
+  type        = string
+  default     = "us-east-1"
+}
+
+# keys to the castle variable
+variable "aws_access_key" {
+  type      = string
+  sensitive = true
+  default   = ""
+}
+
+variable "aws_secret_key" {
+  type      = string
+  sensitive = true
+  default   = ""
+}
+
