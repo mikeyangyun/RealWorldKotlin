3f6f4e67c4d3f452ef7fb8e9524236e5610aee8a
Yunsong <Mikeyangyun@gmail.com>
2023-03-03 00:21:48 +0800

    [yunsong] chore: refresh the .gitignore file

---
+++ src/main/kotlin/com/tw/yys/backendrealworld/BackendRealWorldApplication.kt
@@ -0,0 +1,11 @@
+package com.tw.yys.backendrealworld
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendRealWorldApplication
+
+fun main(args: Array<String>) {
+    runApplication<BackendRealWorldApplication>(*args)
+}
---
+++ src/test/kotlin/com/tw/yys/backendrealworld/BackendRealWorldApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tw.yys.backendrealworld
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendRealWorldApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
