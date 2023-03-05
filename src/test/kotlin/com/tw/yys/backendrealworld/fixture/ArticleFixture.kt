c30c525729877334a8a2199270ac907ef8560c6a
Yunsong <Mikeyangyun@gmail.com>
2023-03-05 23:36:46 +0800

    [yunsong] feat: add ArticleFixture for test

---
+++ src/test/kotlin/com/tw/yys/backendrealworld/fixture/ArticleFixture.kt
@@ -0,0 +1,54 @@
+package com.tw.yys.backendrealworld.fixture
+
+import com.tw.yys.backendrealworld.domain.common.model.UserProfile
+import com.tw.yys.backendrealworld.interfaces.inbound.dto.*
+import java.time.Instant
+
+object ArticleFixture {
+    object Default {
+        val createNewArticleRequest = CreateNewArticleRequest(
+            title = "title",
+            description = "description",
+            content = "content",
+            tags = null
+        )
+
+        val updateArticleRequest =UpdateArticleRequest(
+            title = "title",
+            description = "description",
+            content = "new content"
+        )
+        private val userProfile = UserProfile(
+            username = "jack",
+            bio = null,
+            image = null,
+            following = false
+        )
+
+        val updateArticleResponseDto = SingleArticleProfileResponseDto(
+            author = userProfile,
+            articleId = 1,
+            title = "title",
+            description = "description",
+            content = "new content",
+            tags = null,
+            favoritesCount = null,
+            createdAt = Instant.now(),
+            updatedAt = null
+        )
+
+
+        val singleArticleProfileResponseDto = SingleArticleProfileResponseDto(
+            author = userProfile,
+            articleId = 1,
+            title = "title",
+            description = "description",
+            content = "content",
+            tags = null,
+            favoritesCount = null,
+            createdAt = Instant.now(),
+            updatedAt = null
+        )
+
+    }
+}
