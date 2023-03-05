249b730d1ace4bba965670b879129aeb8cfd7a5a
Yunsong <Mikeyangyun@gmail.com>
2023-03-05 19:19:34 +0800

    [yunsong] refactor: extract duplicate method

--- src/main/kotlin/com/tw/yys/backendrealworld/domain/ArticleService.kt
+++ src/main/kotlin/com/tw/yys/backendrealworld/domain/ArticleService.kt
@@ -1,42 +1,73 @@
 package com.tw.yys.backendrealworld.domain
 
+import com.tw.yys.backendrealworld.domain.common.errors.ArticleNotFoundException
 import com.tw.yys.backendrealworld.domain.common.errors.UserNotFoundException
 import com.tw.yys.backendrealworld.domain.common.model.UserProfile
 import com.tw.yys.backendrealworld.interfaces.inbound.dto.CreateNewArticleRequest
+import com.tw.yys.backendrealworld.interfaces.inbound.dto.SingleArticleProfileResponseDto
+import com.tw.yys.backendrealworld.interfaces.inbound.dto.UpdateArticleRequest
 import com.tw.yys.backendrealworld.interfaces.outbound.article.ArticleEntity
 import com.tw.yys.backendrealworld.interfaces.outbound.article.response.SingleArticleProfileEntity
+import com.tw.yys.backendrealworld.interfaces.outbound.userInfo.UserInfoEntity
 import org.springframework.stereotype.Service
 
 @Service
 class ArticleService(
-    private val repository: ArticleRepository,
-    private val userInfoService: UserInfoService
+    private val articleRepository: ArticleRepository,
+    private val userInfoRepository: UserInfoRepository
 ) {
     fun createNewArticle(userId: String, command: CreateNewArticleRequest): SingleArticleProfileEntity {
-        val existingUser = userInfoService.findUserById(userId) ?: throw UserNotFoundException()
+        val existingUser = userInfoRepository.findUserById(userId) ?: throw UserNotFoundException()
+        val author = getUserProfile(existingUser)
+        val entity = command.toEntity(userId)
+        val savedArticle = articleRepository.save(entity)
+
+        return singleArticleProfileEntity(author, savedArticle)
+    }
+
+    private fun singleArticleProfileEntity(
+        author: UserProfile,
+        savedArticle: ArticleEntity
+    ) = SingleArticleProfileEntity(
+        author = author,
+        articleId = savedArticle.articleId,
+        title = savedArticle.title,
+        description = savedArticle.description,
+        content = savedArticle.content,
+        favoritesCount = savedArticle.favoritesCount,
+        tags = savedArticle.tags,
+        createdAt = savedArticle.createdAt,
+        updatedAt = savedArticle.updatedAt,
+    )
+
+    fun updateArticle(articleId: Long, request: UpdateArticleRequest): SingleArticleProfileResponseDto {
+        val existingArticle = articleRepository.findArticleById(articleId) ?: throw ArticleNotFoundException()
+
+        val existingUser = userInfoRepository.findUserById(existingArticle.authorId) ?: throw UserNotFoundException()
+        val author = getUserProfile(existingUser)
+
+        val updated = existingArticle.copy(
+            title = request.title,
+            description = request.description,
+            content = request.content,
+        )
+        articleRepository.save(updated)
+
+        return singleArticleProfileEntity(author, updated).toDto()
+    }
+
+    private fun getUserProfile(existingUser: UserInfoEntity): UserProfile {
         val author = UserProfile(
             username = existingUser.username,
             bio = existingUser.bio,
             image = existingUser.image
         )
-        val entity = command.toEntity(userId)
-        val savedArticle = repository.save(entity)
-
-        return SingleArticleProfileEntity(
-            author = author,
-            articleId = savedArticle.articleId,
-            title = savedArticle.title,
-            description = savedArticle.description,
-            content = savedArticle.content,
-            favoritesCount = savedArticle.favoritesCount,
-            tags = savedArticle.tags,
-            createdAt = savedArticle.createdAt,
-            updatedAt = savedArticle.updatedAt,
-        )
+        return author
     }
 }
 
 interface ArticleRepository{
     fun save(entity: ArticleEntity): ArticleEntity
+    fun findArticleById(articleId: Long): ArticleEntity?
 
 }
