71be1a614563c34a09ef4f7682234dbc07c6eee7
Yunsong <Mikeyangyun@gmail.com>
2023-03-05 10:12:24 +0800

    [yunsong] feat: add UserInfoResponseDto and UserInfoResponse

--- src/main/kotlin/com/tw/yys/backendrealworld/interfaces/inbound/dto/UserInfoResponseDto.kt
+++ src/main/kotlin/com/tw/yys/backendrealworld/interfaces/inbound/dto/UserInfoResponseDto.kt
@@ -1,9 +1,13 @@
 package com.tw.yys.backendrealworld.interfaces.inbound.dto
 
+import com.tw.yys.backendrealworld.interfaces.outbound.userInfo.response.UserInfoResponse
+
 data class UserInfoResponseDto(
     val username: String,
     val email: String,
     val password: String,
     val bio: String?,
     val image: String?
-)
+){
+    fun toResponse() = UserInfoResponse(this)
+}
---
+++ src/main/kotlin/com/tw/yys/backendrealworld/interfaces/outbound/userInfo/response/UserInfoResponse.kt
@@ -0,0 +1,5 @@
+package com.tw.yys.backendrealworld.interfaces.outbound.userInfo.response
+
+import com.tw.yys.backendrealworld.interfaces.inbound.dto.UserInfoResponseDto
+
+data class UserInfoResponse(val user: UserInfoResponseDto)
