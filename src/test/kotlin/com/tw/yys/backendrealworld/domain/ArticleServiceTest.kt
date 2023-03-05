abb62db9e5e96becc59306c80bbe2abf6032cf6e
Yunsong <Mikeyangyun@gmail.com>
2023-03-06 01:04:01 +0800

    [yunsong] test: add test for updateArticle method in service layer

--- src/test/kotlin/com/tw/yys/backendrealworld/domain/ArticleServiceTest.kt
+++ src/test/kotlin/com/tw/yys/backendrealworld/domain/ArticleServiceTest.kt
@@ -1,5 +1,6 @@
 package com.tw.yys.backendrealworld.domain
 
+import com.tw.yys.backendrealworld.domain.common.errors.ArticleNotFoundException
 import com.tw.yys.backendrealworld.domain.common.errors.UserNotFoundException
 import com.tw.yys.backendrealworld.fixture.ArticleFixture
 import com.tw.yys.backendrealworld.fixture.UserInfoFixture
@@ -30,7 +31,7 @@ class ArticleServiceTest{
 
         @Test
         fun `should throw UserNotFoundException given user not found by userId`() {
-            every { userInfoRepository.findUserById(any()) } returns userInfoEntity
+            every { userInfoRepository.findUserById(any()) } returns null
             every { articleRepository.save(articleEntity) } returns articleEntity
 
             assertThrows<UserNotFoundException> {
@@ -58,6 +59,58 @@ class ArticleServiceTest{
             assertThat(singleArticleProfileEntity.title).isEqualTo(articleEntity.title)
         }
     }
+
+    @Nested
+    inner class WhenUpdateArticle {
+        private val request = ArticleFixture.Default.updateArticleRequest
+        private val articleEntity = ArticleFixture.Default.articleEntity
+        private val userInfoEntity = UserInfoFixture.Default.userInfoEntity
+
+        @Test
+        fun `should throw ArticleNotFoundException given article not found by articleId`() {
+            every { articleRepository.findArticleById(any()) } returns null
+            every { userInfoRepository.findUserById(any()) } returns null
+
+            assertThrows<ArticleNotFoundException> {
+                service.updateArticle(1, request)
+            }
+            verify(inverse = true) {
+                userInfoRepository.findUserById(any())
+                articleRepository.save(any())
+            }
+        }
+
+        @Test
+        fun `should throw UserNotFoundException given user not found by userId`() {
+            every { articleRepository.findArticleById(any()) } returns articleEntity
+            every { userInfoRepository.findUserById(any()) } returns null
+
+            assertThrows<UserNotFoundException> {
+                service.updateArticle(1, request)
+            }
+            verify {
+                articleRepository.findArticleById(1)
+            }
+            verify(inverse = true) {
+                articleRepository.save(any())
+            }
+        }
+
+        @Test
+        fun `should return updated article profile given update article successfully`() {
+            every { articleRepository.findArticleById(any()) } returns articleEntity
+            every { userInfoRepository.findUserById(any()) } returns userInfoEntity
+
+            val singleArticleProfileEntity = service.updateArticle(1, request)
+            verify {
+                articleRepository.save(any())
+            }
+
+            assertThat(singleArticleProfileEntity.content).isEqualTo("new content")
+        }
+    }
+
+
 
 
 }
