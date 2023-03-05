25a800f06953b83bf62b20b5372c5d070b345615
Yunsong <Mikeyangyun@gmail.com>
2023-03-05 16:51:49 +0800

    [yunsong] feat: use annotation @GeneratedValue

--- src/main/kotlin/com/tw/yys/backendrealworld/interfaces/outbound/article/ArticlePo.kt
+++ src/main/kotlin/com/tw/yys/backendrealworld/interfaces/outbound/article/ArticlePo.kt
@@ -1,23 +1,39 @@
 package com.tw.yys.backendrealworld.interfaces.outbound.article
 
-import java.math.BigInteger
+import com.fasterxml.jackson.annotation.JsonIgnore
 import java.time.Instant
 import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.Table
 
 @Entity
 @Table(name = "articles")
 data class ArticlePo(
-    @Id val id: BigInteger,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    val id: Long = 0,
 
     @Column(name = "author_id") val authorId: String,
     @Column(name = "title") var title: String,
     @Column(name = "description") var description: String,
     @Column(name = "content") var content: String,
-    @Column(name = "favorites_count") var favoritesCount: Integer?,
+    @Column(name = "favorites_count") var favoritesCount: Int?,
     @Column(name = "tags") var tags: String?,
     @Column(name = "created_at") val createdAt: Instant?,
     @Column(name = "updated_at") var updatedAt: Instant?
-)
+) {
+    fun toDomain() = ArticleEntity(
+        authorId = authorId,
+        title = title,
+        description = description,
+        content = content,
+        favoritesCount = favoritesCount,
+        tags = tags,
+        createdAt = createdAt,
+        updatedAt = updatedAt,
+    )
+}
