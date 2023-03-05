5ed82d0d51ce4f9b30ee86be9d4a0b4999a652fb
Yunsong <Mikeyangyun@gmail.com>
2023-03-05 16:51:03 +0800

    [yunsong] feat: add ArticleEntity and ArticleResponse

--- src/main/kotlin/com/tw/yys/backendrealworld/interfaces/outbound/article/ArticleEntity.kt
+++ src/main/kotlin/com/tw/yys/backendrealworld/interfaces/outbound/article/ArticleEntity.kt
@@ -1,17 +1,25 @@
 package com.tw.yys.backendrealworld.interfaces.outbound.article
 
-import java.math.BigInteger
 import java.time.Instant
 
 data class ArticleEntity(
-    val id: BigInteger,
     val authorId: String,
     val title: String,
     val description: String,
     val content: String,
-    val favoritesCount: Integer?,
+    val favoritesCount: Int?,
     val tags: String?,
     val createdAt: Instant?,
     val updatedAt: Instant?
 ) {
+    fun toPo() = ArticlePo(
+        authorId = authorId,
+        title = title,
+        description = description,
+        content = content,
+        favoritesCount = favoritesCount,
+        tags = tags,
+        createdAt = createdAt,
+        updatedAt = updatedAt,
+    )
 }
---
+++ src/main/kotlin/com/tw/yys/backendrealworld/interfaces/outbound/article/response/ArticleResponse.kt
@@ -0,0 +1,5 @@
+package com.tw.yys.backendrealworld.interfaces.outbound.article.response
+
+import com.tw.yys.backendrealworld.interfaces.inbound.dto.SingleArticleProfileResponseDto
+
+data class ArticleResponse(val article: SingleArticleProfileResponseDto)
