1f216b290a71e632620b6e795ab7dfba313106af
Yunsong <Mikeyangyun@gmail.com>
2023-03-05 13:06:28 +0800

    [yunsong] feat: add updateUserInfo fixture

--- src/test/kotlin/com/tw/yys/backendrealworld/fixture/UserInfoFixture.kt
+++ src/test/kotlin/com/tw/yys/backendrealworld/fixture/UserInfoFixture.kt
@@ -1,17 +1,19 @@
 package com.tw.yys.backendrealworld.fixture
 
 import com.tw.yys.backendrealworld.interfaces.inbound.dto.CreateNewAccountRequest
+import com.tw.yys.backendrealworld.interfaces.inbound.dto.UpdateUserInfoRequest
 import com.tw.yys.backendrealworld.interfaces.inbound.dto.UserInfoResponseDto
 import com.tw.yys.backendrealworld.interfaces.outbound.userInfo.UserInfoEntity
 
 object UserInfoFixture {
     object Default {
-        val request = CreateNewAccountRequest(
+        val createNewAccountRequest = CreateNewAccountRequest(
             username = "jack",
             email = "jack@gmail.com",
             password = "password"
         )
         val userInfoEntity = UserInfoEntity(
+            id = "fake id for test",
             username = "jack",
             email = "jack@gmail.com",
             password = "password",
@@ -24,6 +26,31 @@ object UserInfoFixture {
             password = "password",
             bio = null,
             image = null
+        )
+        val updateUserInfoRequest = UpdateUserInfoRequest(
+            username = "jack",
+            email = "jack@gmail.com",
+            password = "password",
+            bio ="https://realWorld.bios",
+            image = "image for user"
+        )
+
+        val updateUserInfoEntity = UserInfoEntity(
+            id = "fake id for test",
+            username = "jack",
+            email = "jack@gmail.com",
+            password = "password",
+            bio ="https://realWorld.bios",
+            image = "image for user"
+        )
+
+
+        val updateUserInfoResponseDto = UserInfoResponseDto(
+            username = "jack",
+            email = "jack@gmail.com",
+            password = "password",
+            bio ="https://realWorld.bios",
+            image = "image for user"
         )
     }
 }
